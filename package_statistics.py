265519cf760a3b90713734bff55817c2ae1958c9
DRK-512 <kono6886@kettering.edu>
2022-10-25 22:44:18 -0400

    packages now in packageList

--- package_statistics.py
+++ package_statistics.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-import sys 		# For the input param 
-import wget 	# For wget command on URL
-import pathlib 	# checking if file exists
-import gzip 	# extract .gz file
-import shutil 	# open the content of .gz file
+import sys      # For the input param 
+import wget     # For wget command on URL
+import pathlib  # checking if file exists
+import gzip     # extract .gz file
+import shutil   # open the content of .gz file
+import csv      # reading contents file
 
 ############################################################################
 #                           Helper Functions                               #
 ############################################################################
 
-# this will check the user's input architecture, and see if it is not valid
+# This will check the user's input architecture, and see if it is not valid
 def checkArch(input):
 	if not ((input=="amd64") or (input=="arm64") or (input=="armel") or (input=="armhf") or
 	(input=="i386") or (input=="mips64el") or (input=="mipsel") or (input=="ppc64el") or
 	(input=="s390x")):
-		# If architecture is not valid, we end the script
+		# if architecture is not valid, end the script
 		print(input+" is not a valid architecture, please input a valid one")
 		print("Here is a list of valid architectures for this script: ")
 		print("amd64\narm64\narmel\narmhf\ni386\nmips64el\nmipsel\nppc64el\ns390x")
@@ -39,20 +40,22 @@ def extractor(input):
 			with open('Contents-'+input, 'wb') as f_out:
 				shutil.copyfileobj(f_in, f_out)	
 				
+				
+				
 ############################################################################
 #                             Main Function                                #
 ############################################################################
 
 arch=""
 try: 
-	arch = sys.argv[1] # I only care about the first input
+	arch = sys.argv[1] # only want the first input
 except:
 	# If the user did not input something, they will know  
 	print("Please enter an archtecture whilst running the program")
 	print("EXAMPLE RUN: $ ./package_statistics.py <architecture name> ")
 	quit()
 	
-# first we check if a valid architecture was given
+# First we check if a valid architecture was given
 checkArch(arch)
 
 # Now we will fetch the Contents-arch
@@ -61,6 +64,16 @@ fetcher(arch)
 # Now we extract the package, but first check if it exists
 extractor(arch)
 
+# Now we put all the packages into a list
+packageList = []
+with open('./Contents-'+arch) as file_obj:
+	reader_obj = csv.reader(file_obj)
+	
+	# Iterate each row of the contents file and add package to list
+	for row in reader_obj:
+		spaceIndex = row[0].rfind(" ")+1
+		package = str(row[0][spaceIndex:])
+		packageList.append(package)
 
 
 
